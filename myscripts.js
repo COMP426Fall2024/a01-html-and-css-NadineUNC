55ae703b7768fdf724211594693a650467581803
Nadine Hughes <nadie@ad.unc.edu>
2024-09-10 12:59:38 -0400

    finalize

---
+++ myscripts.js
@@ -0,0 +1,16 @@
+var button = document.getElementsByClassName("submit")[0];
+
+function submitLoginForm(event) {
+  event.preventDefault();
+
+  s = event.target["selection"].value;
+  let text = document.getElementsByClassName("thank")[0];
+
+  if (event.target["selection"].value == " ") {
+    text.innerText = "Please Choose An Option.";
+  } else {
+    text.innerText = "Thank You for choosing " + s + "!";
+    var form = document.getElementById("form");
+    form.style.display = "none"; 
+  }
+}
